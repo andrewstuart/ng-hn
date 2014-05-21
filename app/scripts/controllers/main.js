f942ae3922d72fbe652c69d0b1686c854005099f
Andrew Stuart <andrew.stuart2@gmail.com>
2014-05-21 13:11:34 -0700

    More ui and logic stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,11 @@ angular.module('tempApp')
       'Karma'
     ];
 
+    var next;
+
     $http.get("http://localhost:8000").success(function(data) {
       $scope.stories = data.articles;
+      next = data.next;
     });
 
     $scope.lengthOptions = [10, 20, 30, 40, 50];
@@ -21,8 +24,21 @@ angular.module('tempApp')
     $scope.page = function(num) {
       var newPage = $scope.c.page + num;
 
-      if(newPage >= 0 && newPage * $scope.numStories <= $scope.stories.length + $scope.numStories) {
+      var tooFar = newPage * $scope.numStories >= $scope.stories.length + $scope.numStories;
+
+      if(newPage >= 0 && !tooFar) {
         $scope.c.page = newPage;
+      }
+
+      if((newPage + 1) * $scope.numStories >= $scope.stories.length) {
+        $http.get("http://localhost:8000/next/" + next).success(function(data) {
+          $scope.stories = data.articles;
+          next = data.next;
+
+          while (next[0] === '/') {
+            next = next.substring(1);
+          }
+        });
       }
     };
   }]);
